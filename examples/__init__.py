529bad21b951b4042454046e46dd7699b7f5bb6a
Nyongesa Briton <nyongesabriton620@gmail.com>
2026-07-18 00:12:12 +0300

    Clean up repository structure: fix src/ package duplication, reorganize tests, remove generated files

--- examples/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-GCON Examples
-
-This directory contains example scripts demonstrating GCON usage:
-
-- simple_job.py: Basic job execution
-- pytorch_example.py: PyTorch training job
-- multi_job.py: Running multiple jobs
-"""
